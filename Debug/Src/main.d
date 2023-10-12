4bc9481b45f5b4ccd8ecf308445ac269600f6e24
Eslam Osama Saad Fathallah <101063938+Eslam1Osama@users.noreply.github.com>
2023-10-12 18:55:32 +0300

    Add files via upload

---
+++ Debug/Src/main.d
@@ -0,0 +1,35 @@
+Src/main.o: ../Src/main.c ../Inc/LSTD_TYPES.h ../Inc/LBIT_MATH.h \
+ ../Inc/LSTM32F103C8.h ../Inc/FREERTOS.h ../Inc/FreeRTOSConfig.h \
+ ../Inc/projdefs.h ../Inc/portable.h ../Inc/deprecated_definitions.h \
+ ../Inc/portmacro.h ../Inc/mpu_wrappers.h ../Inc/task.h ../Inc/list.h \
+ ../Inc/FreeRTOSConfig.h ../Inc/MRCC_interface.h ../Inc/MGPIO_interface.h
+
+../Inc/LSTD_TYPES.h:
+
+../Inc/LBIT_MATH.h:
+
+../Inc/LSTM32F103C8.h:
+
+../Inc/FREERTOS.h:
+
+../Inc/FreeRTOSConfig.h:
+
+../Inc/projdefs.h:
+
+../Inc/portable.h:
+
+../Inc/deprecated_definitions.h:
+
+../Inc/portmacro.h:
+
+../Inc/mpu_wrappers.h:
+
+../Inc/task.h:
+
+../Inc/list.h:
+
+../Inc/FreeRTOSConfig.h:
+
+../Inc/MRCC_interface.h:
+
+../Inc/MGPIO_interface.h:
